138ed5c5bf5b29965a02076091aa5c5708a57ad7
Freiii21 <mrfreiii@gmail.com>
2023-03-22 21:47:00 +0300

    Added db settings

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const sequilize = require("./db");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.listen(PORT, () => { console.log(`Server started on port ${PORT}`)});
+const start = async () => {
+    try {
+        await sequilize.authenticate();
+        await sequilize.sync();
+
+        app.listen(PORT, () => { console.log(`Server started on port ${PORT}`)});
+    } catch (e) {
+        console.log(e);
+    }
+}
