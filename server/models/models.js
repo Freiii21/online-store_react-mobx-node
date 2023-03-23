aa0abee43c17cdf964102088b36e38279c49fa26
Freiii21 <mrfreiii@gmail.com>
2023-03-23 22:29:56 +0300

    Added user model

---
+++ server/models/models.js
@@ -0,0 +1,9 @@
+const sequelize = require("../db");
+const { DataType, DataTypes} = require("sequelize");
+
+const User = sequelize.define("user", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: "USER" },
+})
